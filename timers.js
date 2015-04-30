de1c44bad06b86719fa93b5cba2dc4df1be89438
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-29 23:17:41 -0400

    reset and pause

--- timers.js
+++ timers.js
@@ -1,10 +1,13 @@
 // * When "Start" is clicked, the text "Stop Watch" should be replaced by "Time elapsed: 0"
 var start = document.body.querySelector("#start")
 var timer = document.body.querySelector("#timer")
+var reset = document.body.querySelector("#reset")
+var pause = document.body.querySelector("#pause")
 var seconds = 0
+var clock
 
 start.addEventListener("click", function(){
-  var clock = setInterval( function() { countSec() }, 1000)
+  clock = setInterval( function() { countSec() }, 1000)
 })
 
 function countSec(){
@@ -13,4 +16,13 @@ function countSec(){
 }
 
 // * When "Reset" is clicked, the text should return to "Stop Watch"
+reset.addEventListener("click", function(){
+  clearInterval(clock)
+  timer.innerHTML = "Stop Watch"
+  seconds = 0
+})
+
 // * When "Pause" is clicked, the text should say "Time elapsed: 1", but stop incrementing.
+pause.addEventListener("click", function(){
+  clearInterval(clock)
+})
